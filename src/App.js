69fabb3734696e1f75ad6fe1f9451ba963e7356c
Carlos Andres Charris S <darkklitos@gmail.com>
2021-10-23 11:30:35 -0500

    Primer Commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
+import { NavBar } from './components/NavBar.jsx';
+import { Principal } from './components/Principal.jsx';
+import { HolaProvider } from './context/HolaProvider.jsx';
+import { TemaProvider } from './context/TemaProvider.jsx';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TemaProvider className="App">
+      <HolaProvider >
+
+        <NavBar />
+        <Principal />
+
+      </HolaProvider >
+    </TemaProvider>
   );
 }
 
